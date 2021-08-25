c41cbf5c7a2d308dd131beee9ce737a2f3c3e188
Federico Tomassetti <federico@tomassetti.me>
2021-08-25 09:46:17 +0200

    refactor call

--- tests/calls.test.ts
+++ tests/calls.test.ts
@@ -1,11 +1,11 @@
-import { expect } from 'chai';
 import {MPSServerClient} from "../src";
 import WebSocket from 'ws';
 import {CreateIntentionsBlockAnswer} from "../src/messages";
 import {SimpleWSServer} from "./support";
+import * as assert from "assert";
 
 describe('calls', () => {
-    it('call createIntentionsBlock', done => {
+    it('call createIntentionsBlock', async () => {
         const wsServer = new SimpleWSServer(9001, {
             "CreateIntentionsBlock": (data: any, socket: WebSocket) => {
                 socket.send(JSON.stringify({
@@ -22,26 +22,20 @@ describe('calls', () => {
             }
         });
         const client = new MPSServerClient(wsServer.url());
-        client.connect(500).then(res1 => {
-            client.createIntentionsBlock({
+        await client.connect(500)
+        const res = await client.createIntentionsBlock({
                 model: "mymodel.a.b",
                 id: {
                     regularNodeID: "NODE-abc"
                 }
-            }).then( res2 => {
-                wsServer.close();
-                expect(res2 === {
-                    blockUUID: "ebc6f2c6-65dc-4379-bcb5-f82a7c202767",
-                    intentions: [
-                        {index: 0, description: "intention A"},
-                        {index: 1, description: "intention B"}
-                    ]
-                } as CreateIntentionsBlockAnswer);
-
-                done();
-            });
-        }).catch(err => {
-            done(`failed because not connected ${err}`)
         });
+        wsServer.close();
+        assert.deepEqual(res, {
+            blockUUID: "ebc6f2c6-65dc-4379-bcb5-f82a7c202767",
+            intentions: [
+                {index: 0, description: "intention A"},
+                {index: 1, description: "intention B"}
+            ]
+        } as CreateIntentionsBlockAnswer);
     });
 });
