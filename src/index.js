a5f9eee85b18d23c4cc04b280088b9e0db2c9662
Federico Tomassetti <federico@tomassetti.me>
2021-11-17 15:31:25 +0100

    removing committed JS files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.MPSServerClient = exports.BaseWSClient = void 0;
-var BaseWSClient_1 = require("./BaseWSClient");
-exports.BaseWSClient = BaseWSClient_1.BaseWSClient;
-var MPSServerClient_1 = require("./gen/MPSServerClient");
-exports.MPSServerClient = MPSServerClient_1.MPSServerClient;
