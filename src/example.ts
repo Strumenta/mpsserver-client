1340a6124c6b2d052e3c48c159f9c25c8a3d3544
Federico Tomassetti <federico@tomassetti.me>
2021-11-24 09:14:11 +0100

    add example

---
+++ src/example.ts
@@ -0,0 +1,18 @@
+import {MPSServerClient} from ".";
+//import {GetInstancesOfConceptAnswer, ModelInfo, ModuleStatus, NodeInfo} from ".";
+
+const client = new MPSServerClient('ws://localhost:2904/jsonrpc');
+
+async function core() {
+    await client.connect().catch((reason: any)=> {
+        console.error("unable to connect to server", reason);
+        process.exit(1);
+    });
+    client.registerForChanges("com.strumenta.financialcalc.sandbox.company", {
+        onPropertyChange: (event) => {
+            console.log("property changed", event)
+        }
+    })
+    process.exit(0)
+}
+void core();
